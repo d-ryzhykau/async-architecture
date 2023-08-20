4752a9a5159711a37de714bf82552095c3c09a47
Dzmitry Ryzhykau <d.ryzhykau@gmail.com>
2023-08-20 12:28:56 +0200

    Integrate schema registry with services

--- auth/app/event_producer.py
+++ auth/app/event_producer.py
@@ -3,20 +3,44 @@ from dataclasses import dataclass
 from typing import ClassVar, List, Optional
 
 from kafka import KafkaProducer
+from event_schema_registry import EventSchemaRegistry
 
 from .models import User
 from .settings import settings
 
+
+def key_serializer(key):
+    return key.encode()
+
+
+def value_serializer(value):
+    return json.dumps(value).encode()
+
+
 # TODO: don't initialize on import time
-producer = KafkaProducer(bootstrap_servers=settings.kafka_address)
+producer = KafkaProducer(
+    bootstrap_servers=settings.kafka_address,
+    key_serializer=key_serializer,
+    value_serializer=value_serializer,
+)
+event_schema_registry = EventSchemaRegistry()
 
 
 def send_events(events: List["BaseEvent"]):
-    for event in events:
+    events_data = [event.get_event_data() for event in events]
+    for event, event_data in zip(events, events_data):
+        # TODO: accumulate and display all errors
+        event_schema_registry.validate(
+            topic=event.topic,
+            event_name=event.event_name,
+            event_version=event.event_version,
+            event=event_data,
+        )
+    for event, event_data in zip(events, events_data):
         producer.send(
             topic=event.topic,
-            key=event.key.encode(),
-            value=event.to_json().encode(),
+            key=event.key,
+            value=event_data,
         )
     producer.flush()
 
@@ -30,14 +54,13 @@ class BaseEvent:
     key: str
     data: Optional[dict]
 
-    def to_json(self) -> str:
-        return json.dumps(
-            {
-                "event_name": self.event_name,
-                "event_version": self.event_version,
-                "data": self.data,
-            }
-        )
+    # TODO: better method name
+    def get_event_data(self) -> dict:
+        return {
+            "event_name": self.event_name,
+            "event_version": self.event_version,
+            "data": self.data,
+        }
 
 
 @dataclass
