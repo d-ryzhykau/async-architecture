d04194dd3f3d5932d3b35620f5cc6f3840ec0ba9
Dzmitry Ryzhykau <d.ryzhykau@gmail.com>
2023-09-25 01:53:03 +0200

    Rename Django project config folder

--- auth/config/settings.py
+++ auth/config/settings.py
@@ -56,7 +56,7 @@ MIDDLEWARE = [
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-ROOT_URLCONF = "auth.urls"
+ROOT_URLCONF = "config.urls"
 
 TEMPLATES = [
     {
@@ -74,7 +74,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = "auth.wsgi.application"
+WSGI_APPLICATION = "config.wsgi.application"
 
 
 # Database
