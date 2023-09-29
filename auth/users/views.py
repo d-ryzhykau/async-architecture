f585ad223dbc245a444845d870a8b8a59ec0e1d3
Dzmitry Ryzhykau <d.ryzhykau@gmail.com>
2023-09-29 21:25:05 +0200

    Add app views and templates

--- auth/users/views.py
+++ auth/users/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.views.generic.base import TemplateView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth import views as auth_views
 
-# Create your views here.
+
+class LoginView(auth_views.LoginView):
+    template_name = "login.html"
+
+
+class LogoutView(auth_views.LogoutView):
+    template_name = "logged_out.html"
+
+
+class IndexView(LoginRequiredMixin, TemplateView):
+    template_name = "index.html"
