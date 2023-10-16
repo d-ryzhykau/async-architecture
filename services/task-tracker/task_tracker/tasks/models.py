6fa344e25ac5c49c19c9e977d2e57d0d2546ea86
Dzmitry Ryzhykau <d.ryzhykau@gmail.com>
2023-10-16 02:08:29 +0200

    Add Task model and migration

--- services/task-tracker/task_tracker/tasks/models.py
+++ services/task-tracker/task_tracker/tasks/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class Task(models.Model):
+    class Status(models.TextChoices):
+        ASSIGNED = "assigned", _("Assigned")
+        COMPLETED = "completed", _("Completed")
+
+    public_id = models.UUIDField(unique=True)
+
+    title = models.CharField(max_length=128)
+    jira_id = models.CharField(max_length=24, blank=True)
+    description = models.TextField(blank=True)
+    status = models.CharField(
+        max_length=16,
+        choices=Status.choices,
+        default=Status.ASSIGNED,
+    )
+    assignee = models.ForeignKey(
+        "users.User",
+        related_name="tasks",
+        on_delete=models.RESTRICT,
+    )
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = "task"
