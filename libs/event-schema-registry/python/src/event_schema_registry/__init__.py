5ac0d2a15a57b38ed1cdb92959a147ec66e82a39
Dzmitry Ryzhykau <d.ryzhykau@gmail.com>
2023-11-08 01:43:25 +0100

    event_schema_registry improvements
    Decouple lib exceptions from underlying deps.
    Add docstring to validate method.
    Add comment for implicit method binding to instance.

--- libs/event-schema-registry/python/src/event_schema_registry/__init__.py
+++ libs/event-schema-registry/python/src/event_schema_registry/__init__.py
@@ -1,3 +1,4 @@
+import contextlib
 import json
 from functools import lru_cache
 from importlib.resources import files
@@ -5,12 +6,25 @@ from typing import Optional
 
 from jsonschema.protocols import Validator
 from jsonschema.validators import validator_for
+from jsonschema.exceptions import ValidationError
+
+
+class EventSchemaValidationError(Exception):
+    """Event validation against schema registry has failed."""
+
+
+class EventSchemaNotFound(Exception):
+    """Event schema was not found."""
 
 
 # TODO: move common schemas to registry for reuse
 class EventSchemaRegistry:
     def __init__(self, validator_cache_size=100):
-        self._get_validator = lru_cache(validator_cache_size)(self._get_validator)
+        self._get_validator = (
+            # add method wrapped in lru_cache as instance attribute
+            # to achieve instance level caching
+            lru_cache(validator_cache_size)(self._get_validator)
+        )
 
     @staticmethod
     def _get_schema(topic: str, event_name: str, event_version: int):
@@ -20,7 +34,11 @@ class EventSchemaRegistry:
             .joinpath(event_name)
             .joinpath(f"{event_version}.json")
         )
-        with schema_path.open("r") as schema_file:
+        try:
+            schema_file = schema_path.open("r")
+        except FileNotFoundError:
+            raise EventSchemaNotFound
+        with contextlib.closing(schema_file):
             return json.load(schema_file)
 
     def _get_validator(
@@ -45,6 +63,21 @@ class EventSchemaRegistry:
         event: Optional[dict] = None,
         data: Optional[dict] = None,
     ):
+        """Validates event data against schema registry.
+
+        Args:
+            topic: name of the event topic.
+            event_name: name of the event.
+            event_version: version of the event.
+            event: (deprecated, use `data` instead) complete event payload
+                including its name, version and extra data.
+            data: extra payload attached to the event.
+
+        Raises:
+            EventSchemaNotFound: if schema for given `topic`, `event_name`
+                and `event_version` could not be found.
+            EventSchemaValidationError: if event schema validation failed.
+        """
         data_sources = [event, data]
         if not any(data_sources) or all(data_sources):
             raise ValueError("either 'event' or 'data' must be provided")
@@ -56,8 +89,12 @@ class EventSchemaRegistry:
                 "data": data,
             }
 
-        self._get_validator(
+        validator = self._get_validator(
             topic=topic,
             event_name=event_name,
             event_version=event_version,
-        ).validate(event)
+        )
+        try:
+            validator.validate(event)
+        except ValidationError:
+            raise EventSchemaValidationError
