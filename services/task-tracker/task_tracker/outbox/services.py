8b418bc0642aff383a71c4240bee240b94c2d9d8
Dzmitry Ryzhykau <d.ryzhykau@gmail.com>
2023-11-09 02:03:47 +0100

    outbox: event_bulk_create
    Replace single event creation function with bulk creation alternative.

--- services/task-tracker/task_tracker/outbox/services.py
+++ services/task-tracker/task_tracker/outbox/services.py
@@ -1,26 +1,36 @@
-from typing import Any
+from typing import Sequence
 
-from event_schema_registry import EventSchemaRegistry
+from event_schema_registry import (
+    EventSchemaRegistry,
+    EventSchemaNotFound,
+    EventSchemaValidationError,
+)
 
 from .models import Event
 
 event_schema_registry = EventSchemaRegistry()
 
 
-def event_create(
-    topic: str,
-    name: str,
-    version: int,
-    key: str,
-    data: Any,
-) -> Event:
-    """Validate `event` against schema registry and store it."""
-    event_schema_registry.validate(
-        topic=topic,
-        event_name=name,
-        event_version=version,
-        data=data,
-    )
-    event = Event(topic=topic, name=name, version=version, key=key, data=data)
-    event.save()
-    return event
+def event_bulk_create(events: Sequence[Event]):
+    """Validate `events` against schema registry.
+    If all events are valid, store them.
+
+    Raises:
+        ExceptionGroup: raised with all errors encountered on event validation.
+    """
+    validation_exceptions = []
+    for event in events:
+        try:
+            event_schema_registry.validate(
+                topic=event.topic,
+                key=event.key,
+                event_name=event.event_name,
+                event_version=event.event_version,
+                data=event.data,
+            )
+        except (EventSchemaNotFound, EventSchemaValidationError) as exc:
+            validation_exceptions.append(exc)
+    if validation_exceptions:
+        raise ExceptionGroup("event_validation_exceptions", validation_exceptions)
+
+    Event.objects.bulk_create(events)
