ffcce9c6bf01e2820a24540782f38a1b3278d4ce
Dzmitry Ryzhykau <d.ryzhykau@gmail.com>
2023-11-09 02:04:45 +0100

    Reorder and group Event fields

--- services/task-tracker/task_tracker/outbox/models.py
+++ services/task-tracker/task_tracker/outbox/models.py
@@ -14,11 +14,14 @@ class Event(models.Model):
     objects = models.Manager.from_queryset(EventQuerySet)()
 
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+
     topic = models.CharField(max_length=255)
+    key = models.CharField(max_length=255)
+
     name = models.CharField(max_length=255)
     version = models.PositiveSmallIntegerField()
-    key = models.CharField(max_length=255)
     data = models.JSONField()
+
     sent_at = models.DateTimeField(blank=True, null=True)
 
     class Meta:
